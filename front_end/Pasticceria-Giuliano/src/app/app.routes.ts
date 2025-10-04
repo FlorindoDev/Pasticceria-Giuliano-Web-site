e22f046afde31ae7cc30c76c263e0246d2cea3b1
filix <florindozec@gmail.com>
2025-10-04 16:24:06 +0000

    preview homepoge

--- front_end/Pasticceria-Giuliano/src/app/app.routes.ts
+++ front_end/Pasticceria-Giuliano/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    },
+    {
+        path: "home",
+        title: "Home Page",
+        component: Homepage
+    }
+
+];
