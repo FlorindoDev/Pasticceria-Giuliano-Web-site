5ef8b8098154e846c35a89155fe171b08a0be5d3
filix <florindozec@gmail.com>
2025-10-20 20:07:03 +0200

    init carrello

--- front_end/Pasticceria-Giuliano/src/app/app.routes.ts
+++ front_end/Pasticceria-Giuliano/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage.component';
 import { Assortimento } from './assortimento/assortimento.component';
+import { Cart } from './cart/cart.component';
 
 export const routes: Routes = [
 
@@ -18,6 +19,11 @@ export const routes: Routes = [
         path: "assortimento",
         title: "Assortimento",
         component: Assortimento
+    },
+    {
+        path: "cart",
+        title: "Carrello",
+        component: Cart
     }
 
 ];
---
+++ front_end/Pasticceria-Giuliano/src/app/cart/cart.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Login } from '../login/login.component';
+import { SignUp } from '../sign-up/sign-up.component';
+
+
+
+@Component({
+  selector: 'cart',
+  imports: [],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss'
+})
+export class Cart {
+
+
+  constructor(
+
+  ) { }
+
+
+}
